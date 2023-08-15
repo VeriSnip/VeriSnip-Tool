6ff527942996ccef0aa894b8044fef3410e0249f
Pedro Antunes <pedronmantunes@gmail.com>
2023-08-15 21:40:59 +0100

    VTbuild: init scripting.

---
+++ VTbuild.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def search():
+    print("Find or generate '.vs' and modules used by the system.")
+
+def organize():
+    print("Create build directory, substitute '.vs' and copy modules used by the system.")
+
+# Check if this script is called directly
+if __name__ == "__main__":
+    search()
+    organize()
