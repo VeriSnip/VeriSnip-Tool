3911579f4646158fd4cf1f7d660f8b6dbdc11c52
Pedro Antunes <pedronmantunes@gmail.com>
2023-08-16 19:44:21 +0100

    MyReg.v, reg.py: small fixes.

--- library/modules/MyReg.v
+++ library/modules/MyReg.v
@@ -7,11 +7,11 @@ module MyReg #(
     parameter integer DATA_W  = 21,
     parameter integer RST_VAL = {DATA_W{1'b0}}
 ) (
-    `include "clk_en_rst_i.vs"
-    input  wire              en_i,
-    input  wire              rst_i,
-    input  wire [DATA_W-1:0] data_i,
-    output reg  [DATA_W-1:0] data_o
+    `include "io_clk_en_rst.vs"
+    input  wire              data_e,
+    input  wire              data_r,
+    input  wire [DATA_W-1:0] data_d,
+    output reg  [DATA_W-1:0] data_q
 );
 
   `include "reg_data.vs" // en rst
