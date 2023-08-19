c1853dca1a04a60e45404306c0a916629e14d56b
Pedro Antunes <pedronmantunes@gmail.com>
2023-08-19 23:50:54 +0100

    VTcolors: write Done instead of Pass.

--- library/scripts/VTcolors.py
+++ library/scripts/VTcolors.py
@@ -1,7 +1,7 @@
 HEADER = "\033[95m"
 OKBLUE = "\033[94m"  # Blue
 INFO = "\033[96mInfo: "  # Cyan
-OK = "\033[92mPass: "  # Green
+OK = "\033[92mDone: "  # Green
 WARNING = "\033[93mWarning: "  # Orange
 ERROR = "\033[91mError: "  # Red
 NORMAL = "\033[0m"  # White
