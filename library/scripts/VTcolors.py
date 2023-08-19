c6fd98be864e595c22e1183fd81763c8bc838a21
Pedro Antunes <pedronmantunes@gmail.com>
2023-08-19 22:39:14 +0100

    VTcolors: add, for pretty prints.

---
+++ library/scripts/VTcolors.py
@@ -0,0 +1,17 @@
+HEADER = "\033[95m"
+OKBLUE = "\033[94m"  # Blue
+INFO = "\033[96mInfo: "  # Cyan
+OK = "\033[92mPass: "  # Green
+WARNING = "\033[93mWarning: "  # Orange
+ERROR = "\033[91mError: "  # Red
+NORMAL = "\033[0m"  # White
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+
+
+#  Prints the given string with the given text modifier.
+#  Args:
+#    modifier: The text modifier.
+#    string: The string to print.
+def print_color(modifier, string):
+    print(modifier+string+NORMAL)
