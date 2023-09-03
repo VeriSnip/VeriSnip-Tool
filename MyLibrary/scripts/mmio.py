bce19e892890a514893d6f097de68b11cc9670d8
Pedro Antunes <pedronmantunes@gmail.com>
2023-09-03 23:14:01 +0100

    *: reorganize repository.

---
+++ MyLibrary/scripts/mmio.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Should be called as "`include "mmio_{module}.vs" /*
+#                        Reg_name0, Address, Size, Reset Value, Access Type, Reg_reset, Reg_enable
+#                        Reg_name1, Address, Size, Reset Value, Access Type, Reg_reset, Reg_enable
+#                        Reg_name2, Address, Size, Reset Value, Access Type, Reg_reset, Reg_enable
+#                        ...
+#                        */
