46a4777cb6d641bab05dc3c57bb2ba9b0495be26
Pedro Antunes <pedronmantunes@gmail.com>
2023-10-29 01:57:05 +0100

    mmio.py: generate different selects for read and write.

--- MyLibrary/scripts/mmio.py
+++ MyLibrary/scripts/mmio.py
@@ -43,7 +43,7 @@ class memory_mapped_register:
         self.set_address(properties[6])
         self.set_access_type(properties[7])
         self.set_default_value(properties[8])
-        self.set_sel("")
+        self.set_sel()
 
     def set_address(self, mm_reg_address):
         if "address=" in mm_reg_address:
@@ -66,13 +66,9 @@ class memory_mapped_register:
         else:
             self.default_value = mm_reg_default_value
 
-    def set_sel(self, mm_reg_sel):
-        if "select=" in mm_reg_sel:
-            mm_reg_sel = mm_reg_sel.split("=")[1]
-        if mm_reg_sel == "":
-            self.sel = f"{self.reg.name}_sel"
-        else:
-            self.sel = mm_reg_sel
+    def set_sel(self):
+        self.r_sel = f"r_{self.reg.name}_sel"
+        self.w_sel = f"w_{self.reg.name}_sel"
 
 
 def custom_split(description):
@@ -139,10 +135,10 @@ def find_script():
 def sel_registers_desc(mm_reg_list):
     sel_reg_desc = ""
     for mm_reg in mm_reg_list:
-        if mm_reg.reg.en != None:
-            sel_reg_desc += f"  assign {mm_reg.sel} = (address == {mm_reg.address}) & ({mm_reg.reg.en});\n"
-        else:
-            sel_reg_desc += f"  assign {mm_reg.sel} = (address == {mm_reg.address});\n"
+        if "W" in mm_reg.access_type:
+            sel_reg_desc += f"  assign {mm_reg.w_sel} = (w_address == {mm_reg.address}){f' & ({mm_reg.reg.en})' if mm_reg.reg.en != None else ''};\n"
+        if "R" in mm_reg.access_type:
+            sel_reg_desc += f"  assign {mm_reg.r_sel} = (r_address == {mm_reg.address}){f' & ({mm_reg.reg.en})' if mm_reg.reg.en != None else ''};\n"
     return sel_reg_desc
 
 
@@ -151,10 +147,10 @@ def write_registers_desc(mm_reg_list):
     w_desc += "  always @(*) begin\n"
     for mm_reg in mm_reg_list:
         w_desc += f"    {mm_reg.reg.next} = {mm_reg.default_value};\n"
-    w_desc += f"    if (write_enable) begin\n"
+    w_desc += f"    if (w_enable) begin\n"
     for mm_reg in mm_reg_list:
         if "W" in mm_reg.access_type:
-            w_desc += f"      if ({mm_reg.sel}) begin\n"
+            w_desc += f"      if ({mm_reg.w_sel}) begin\n"
             w_desc += f"        {mm_reg.reg.next} = w_data;\n"
             w_desc += "      end\n"
     w_desc += "    end\n"
@@ -166,12 +162,12 @@ def read_registers_desc(mm_reg_list):
     r_desc = "  // Read memory mapped register always block\n"
     r_desc += "  always @(*) begin\n"
     r_desc += "    r_data = 0;\n"
-    r_desc += f"    if (read_enable) begin\n"
+    r_desc += f"    if (r_enable) begin\n"
     for mm_reg in mm_reg_list:
         if "R" in mm_reg.access_type:
-            r_desc += f"      if ({mm_reg.sel}) begin\n"
+            r_desc += f"      if ({mm_reg.r_sel}) begin\n"
             r_desc += (
-                "        r_data = {{(DATA_W-"
+                "        r_data = {{(DATA_WIDTH-"
                 + f"{mm_reg.reg.size}"
                 + "){1'b0}}, "
                 + f"{mm_reg.reg.signal}"
@@ -185,16 +181,18 @@ def read_registers_desc(mm_reg_list):
 
 def generate_mmio_wires(mm_reg_list):
     current_directory = os.getcwd()
-    regs = "r_data, DATA_W\n"
-    wires = "w_data, DATA_W\n"
-    wires += "read_enable, \n"
-    wires += "write_enable, \n"
-    wires += "address, ADDR_W\n"
+    regs = "r_data, DATA_WIDTH\n"
+    wires = "w_data, DATA_WIDTH\n"
+    wires += "r_address, ADDR_WIDTH\n"
+    wires += "w_address, ADDR_WIDTH\n"
+    wires += "r_enable, \n"
+    wires += "w_enable, \n"
     for mm_reg in mm_reg_list:
-        wires += f"{mm_reg.sel}, \n"
-    for mm_reg in mm_reg_list:
+        if "W" in mm_reg.access_type:
+            wires += f"{mm_reg.w_sel}, \n"
+        if "R" in mm_reg.access_type:
+            wires += f"{mm_reg.r_sel}, \n"
         regs += f"{mm_reg.reg.next}, {mm_reg.reg.size}\n"
-    for mm_reg in mm_reg_list:
         regs += f"{mm_reg.reg.signal}, {mm_reg.reg.size}\n"
     scripts, _ = find_verilog_and_scripts(current_directory)
     script_path = find_filename_in_list("generated_wires.py", scripts)
