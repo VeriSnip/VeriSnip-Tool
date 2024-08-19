3747bc3d5fd4c985c9fbd8cccdafa8259a457091
Pedro Antunes <pedronmantunes@gmail.com>
2024-08-19 16:15:13 +0200

    (fix) vt_colors.py: 10/10 in pylint

--- vt_colors.py
+++ vt_colors.py
@@ -5,22 +5,20 @@ import os
 
 # Based on ANSI escape code
 OKBLUE = "\033[94m"  # Blue
-INFO = f"\033[96mInfo"  # Cyan
-OK = f"\033[92mDone"  # Green
-WARNING = f"\033[93mWarning"  # Orange
-ERROR = f"\033[91mError"  # Red
-DEBUG = f"\033[95mDebug"  # Magenta
+INFO = "\033[96mInfo"  # Cyan
+OK = "\033[92mDone"  # Green
+WARNING = "\033[93mWarning"  # Orange
+ERROR = "\033[91mError"  # Red
+DEBUG = "\033[95mDebug"  # Magenta
 NORMAL = "\033[0m"  # White
 BOLD = "\033[1m"
 UNDERLINE = "\033[4m"
 
-"""
-Prints the given string with the given text modifier.
-Args:
-  modifier: The text modifier.
-  string: The string to print.
-"""
 def print_coloured(modifier, string):
+    """This function prints the given string with the given text modifier.
+    Args:
+        modifier: The text modifier.
+        string: The string to print."""
     script_name = os.path.basename(sys.argv[0])
     # Check conditions for printing based on arguments and modifier
     if modifier == DEBUG:
