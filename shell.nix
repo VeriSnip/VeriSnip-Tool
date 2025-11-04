ba81447f70b3ce4bf6879b2a9339272194f646af
Pedro Antunes <pedronmantunes@gmail.com>
2025-11-04 18:34:10 +0100

    NEW nix shell for development

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  name = "my-package-dev-env";
+
+  buildInputs = [
+    pkgs.python3
+    pkgs.python3Packages.virtualenv
+  ];
+
+  shellHook = ''
+    echo Entering pure nix-shell...
+    export PYTHONPATH=
+    export PATH=$PWD/.venv/bin:$PATH
+    virtualenv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip setuptools wheel
+    pip install -e .  # install your package in editable mode
+  '';
+}
+
